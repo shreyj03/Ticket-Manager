ad1ab007aa575f7a43a08a6eca6b0082ccf7793d
Shrey Jain <156033848+shreyj03@users.noreply.github.com>
2024-07-18 15:44:30 +0530

    Update apiSlice.js

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://ticketmanager-api.onrender.com',
+    baseUrl: 'https://ticket-manager-api-6n0d.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
